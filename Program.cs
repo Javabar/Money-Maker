7af24b057b971cee8c91b195a9af9c0a4e71e667
Abdelbar Benaida <abdelbar94@hotmail.co.uk>
2022-09-10 19:21:31 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Welcome to Money Maker!");
+
+Console.WriteLine("Enter an amount to convert to coins:");
+string amount  = Console.ReadLine();
+
+double convertedAmount = Convert.ToDouble(amount);
+Console.WriteLine($"Type your amount again, {convertedAmount} cents is equal to:");
+string example  = Console.ReadLine();
+
+int goldValue = 10;
+int silverValue = 5;
+int bronzeValue = 1;
+
+double goldCoins = Math.Floor(convertedAmount/goldValue);
+double goldLeftOver = goldCoins % goldValue;
+Console.WriteLine($"{goldLeftOver} Gold coin");
+
+double silverCoins = Math.Floor(goldLeftOver/silverValue);
+double silverLeftOver = silverCoins % silverValue;
+Console.WriteLine($"{silverLeftOver} Silver coin");
+
+double bronzeCoins = Math.Floor(silverLeftOver/bronzeValue);
+double bronzeLeftOver = bronzeCoins % bronzeValue;
+Console.WriteLine($"{bronzeLeftOver} Silver coin");
